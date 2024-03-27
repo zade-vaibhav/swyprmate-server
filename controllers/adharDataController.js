d856ccaf84041e4f2681141f2837be252eaa4f1d
vaibhav <vaibhavzade802@gmail.com>
2024-03-27 15:18:40 +0530

    adding success in responce

--- controllers/adharDataController.js
+++ controllers/adharDataController.js
@@ -7,15 +7,15 @@ const { user } = require("../models/users");
 
    const data=await user.findOne({email})
     if(!data){
-        return res.status(404).json({message:"user not found"})
+        return res.status(404).json({success:false,message:"user not found"})
     }
    data.kyc.aadhar.aadhar_data=adharData;
    data.kyc.aadhar.Validity_status=true;
    data.save()
-   return res.status(200).json({message:"user Updated successful!!",data})
+   return res.status(200).json({success:true,message:"user Updated successful!!",data})
     }
     catch(err){
-
+     console.log(err.message)
     }
 }
 
